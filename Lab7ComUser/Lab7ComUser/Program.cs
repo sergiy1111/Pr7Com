2e4642797ca16b1ca54f4cec1833e282c6cfe2b2
sergiy1111 <grigha71@gmail.com>
2021-12-11 12:10:34 +0200

    Fix

--- Lab7ComUser/Lab7ComUser/Program.cs
+++ Lab7ComUser/Lab7ComUser/Program.cs
@@ -34,18 +34,18 @@ namespace Lab7ComUser
             byte[] bytes = new byte[1024];
             // Соединяемся с удаленным устройством
             // Устанавливаем удаленную точку для сокета
-            IPHostEntry ipHost = Dns.GetHostEntry("192.168.1.159"); IPAddress ipAddr = ipHost.AddressList[0];
-            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port); Socket sender = new Socket(ipAddr.AddressFamily,
-            SocketType.Stream, ProtocolType.Tcp);
+            IPHostEntry ipHost = Dns.GetHostEntry("192.168.1.159"); 
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port); 
+            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
             // Соединяем сокет с удаленной точкой
             sender.Connect(ipEndPoint);
 
-            Console.Write("Введите сообщение: ");
+            Console.Write("Введіть повідомлення: ");
             string message = Console.ReadLine();
 
-            Console.WriteLine("Сокет	соединяется	с	{0}	",
-            sender.RemoteEndPoint.ToString());
+            Console.WriteLine("Сокет підключається до	{0}	", sender.RemoteEndPoint.ToString());
             byte[] msg = Encoding.UTF8.GetBytes(message);
             // Отправляем данные через сокет
             int bytesSent = sender.Send(msg);
@@ -53,14 +53,16 @@ namespace Lab7ComUser
             // Получаем ответ от сервера
             int bytesRec = sender.Receive(bytes);
 
-            Console.WriteLine("\nОтвет	от	сервера:	{0}\n\n", Encoding.UTF8.GetString(bytes, 0, bytesRec));
+            Console.WriteLine("\nВідповідь від сервера	{0}\n\n", Encoding.UTF8.GetString(bytes, 0, bytesRec));
 
             //	Используем	рекурсию	для	неоднократного	вызова
-            SendMessageFromSocket(11000);
+            SendMessageFromSocket();
             if (message.IndexOf("<TheEnd>") == -1)
                 SendMessageFromSocket(port);
 
-            // Освобождаем сокет sender.Shutdown(SocketShutdown.Both); sender.Close();
+            // Освобождаем сокет
+            sender.Shutdown(SocketShutdown.Both); 
+            sender.Close();
         }
     }
 }
